12fca0ac064611aaef7c95a9065846a6bc450756
yoadwo <yoadw20@gmail.com>
2018-09-28 17:25:28 +0300

    MapsFragment: add fragment

--- app/src/main/java/com/gingos/dudewheresmycar/MainActivity.java
+++ app/src/main/java/com/gingos/dudewheresmycar/MainActivity.java
@@ -3,9 +3,6 @@ package com.gingos.dudewheresmycar;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -25,7 +22,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private DrawerLayout mDrawerLayout;
 
-    private String currentFragment;
     private HomeFragment homeFragment;
     private CameraFragment cameraFragment;
     private NavigationFragment navigationFragment;
