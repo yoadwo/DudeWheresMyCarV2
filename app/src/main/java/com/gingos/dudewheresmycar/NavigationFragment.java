836771101d441674e45995045754956b0604eb44
yoadwo <yoadw20@gmail.com>
2018-09-28 17:48:52 +0300

    Manifest: remove API key

--- app/src/main/java/com/gingos/dudewheresmycar/NavigationFragment.java
+++ app/src/main/java/com/gingos/dudewheresmycar/NavigationFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,5 +19,18 @@ public class NavigationFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView: ");
         return inflater.inflate(R.layout.fragment_navigation, container, false);
+    }
+
+    // This event is triggered soon after onCreateView(), only if returned view is non-null.
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        insertNestedFragment();
+    }
+
+    // Embeds the child fragment dynamically
+    private void insertNestedFragment() {
+        Fragment childFragment = new MapsFragment();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.child_fragment_map_container, childFragment).commit();
     }
 }
