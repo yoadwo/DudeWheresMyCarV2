beb569b76f33d83ad4c2d9952fedb8f2b6278243
yoadwo <yoadw20@gmail.com>
2018-09-30 14:10:37 +0300

    MapsFragment: added async tasks & fusedLocation

--- app/src/main/java/com/gingos/dudewheresmycar/MapsFragment.java
+++ app/src/main/java/com/gingos/dudewheresmycar/MapsFragment.java
@@ -4,6 +4,7 @@ package com.gingos.dudewheresmycar;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -17,12 +18,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 // with https://developers.google.com/maps/documentation/android-sdk/current-place-tutorial
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
@@ -30,9 +35,14 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     private static final String TAG = "DUDE_nav_mapFragment";
 
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 100;
+    private static final int DEFAULT_ZOOM = 15;
 
     private boolean _locationPermissionGranted;
     private SupportMapFragment _supportMapFragment;
+    private GoogleMap _googleMap;
+    private Location _lastKnownLocation; // Last-known location retrieved by the Fused Location Provider.
+    private FusedLocationProviderClient _fusedLocationProviderClient; // The entry point to the Fused Location Provider.
+    private final LatLng _defaultLocation = new LatLng(-33.8523341, 151.2106085);
 
     public MapsFragment() {
         // Required empty public constructor
@@ -41,8 +51,18 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //build the map
         _supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        permissionRequestCycle_location();
+        if (_supportMapFragment == null){
+            Log.d(TAG, "onCreate: " + "supportMapFragment was null");
+            _supportMapFragment = new SupportMapFragment();
+            FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+            ft.replace(R.id.map, _supportMapFragment).commit();
+        }
+        _supportMapFragment.getMapAsync(this);
+
+        // Construct a FusedLocationProviderClient.
+        _fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
 
     }
 
@@ -58,34 +78,30 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        // Add a marker in Sydney, Australia,
-        // and move the map's camera to the same location.
-        LatLng sydney = new LatLng(-33.852, 151.211);
-        googleMap.addMarker(new MarkerOptions().position(sydney)
-                .title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        if (_locationPermissionGranted){
-            googleMap.setMyLocationEnabled(true);
-        }
+
+        Log.d(TAG, "onMapReady: ");
+        _googleMap = googleMap;
+        // Prompt the user for permission.
+        permissionRequestCycle_location();
+
+        // Turn on the My Location layer and the related control on the map.
+        updateLocationUI();
+
+        // Get the current location of the device and set the position of the map.
+        getDeviceLocation();
     }
 
     // check-explain-request permission cycle for location
     // if granted, will get map for the supportMapFragment
     // if not granted, show dialog
     private void permissionRequestCycle_location(){
-        if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            Log.d(TAG, "onCreateView: has GPS fine location permission");
+        if(ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            Log.d(TAG, "onMapReady: " + "permissionRequestCycle_location" + "has GPS fine location permission");
             _locationPermissionGranted = true;
-            if (_supportMapFragment == null){
-                Log.d(TAG, "onCreateView: " + "supportMapFragment was null");
-                _supportMapFragment = new SupportMapFragment();
-                FragmentTransaction ft = getChildFragmentManager().beginTransaction();
-                ft.replace(R.id.map, _supportMapFragment).commit();
-            }
-            _supportMapFragment.getMapAsync(this);
 
         }else{
-            Log.d(TAG, "onCreateView: no permission granted yet for GPS fine location");
+            Log.d(TAG, "onMapReady: " + "permissionRequestCycle_location" + "no permission granted yet for GPS fine location");
             _locationPermissionGranted = false;
             ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
         }
@@ -108,4 +124,60 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
             }
         }
     }
+
+    // if location permission is granted, add "current location" button to map UI
+    // if not granted, ask for it
+    private void updateLocationUI(){
+        if (_googleMap == null) {
+            return;
+        }
+        try {
+            if (_locationPermissionGranted) {
+                _googleMap.setMyLocationEnabled(true);
+                _googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                _googleMap.setMyLocationEnabled(false);
+                _googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+                _lastKnownLocation = null;
+                permissionRequestCycle_location();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    // ASYNC - use fusedLocations to retrieve device location
+    private void getDeviceLocation(){
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (_locationPermissionGranted) {
+                Task locationResult = _fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(onCompleteListener);
+            }
+        } catch(SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+
+    }
+
+    private OnCompleteListener onCompleteListener = new OnCompleteListener<Location>() {
+        @Override
+        public void onComplete(@NonNull Task<Location> task) {
+            if (task.isSuccessful()) {
+                // Set the map's camera position to the current location of the device.
+                _lastKnownLocation = task.getResult();
+                _googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                        new LatLng(_lastKnownLocation.getLatitude(),
+                                _lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+            } else {
+                Log.d(TAG, "Current location is null. Using defaults.");
+                Log.e(TAG, "Exception: %s", task.getException());
+                _googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(_defaultLocation, DEFAULT_ZOOM));
+                _googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+            }
+        }
+    };
 }
